f58c1ceaf5b9307b6c4523706cd1582346f56d2f
Benjoun <benjoun@gmail>
2018-04-12 17:50:36 +0200

    setting gulp automation / Css architecture / styling first sections [large hero/our beginning]/ Mobile first

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();  // we import the method
+
+gulp.task('watch', function() {
+  
+  browserSync.init({
+    notify: false, // so we don't see the annoying box of browser-sync
+    server: {
+      baseDir: "./app"
+    }
+  });
+   
+  watch('./app/index.html', function() {
+    browserSync.reload();
+    // gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+});
+
+
+// the second argument is a dependency .. the thing to do before the function
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+            .pipe(browserSync.stream());
+})
